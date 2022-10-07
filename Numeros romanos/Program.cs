5ad94ae5beded2b58fc1d87d0d0713880153e74f
labinf1.pasto <labinf1.pasto@PAS-W-L1PC7-HPF>
2022-10-07 17:57:34 -0500

    Agregar archivos de proyecto.

---
+++ Numeros romanos/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Numeros_Romanos
+{
+    public class program
+    {
+        public static void Main()
+        {
+            double number,M, C, D, u;
+            Console.WriteLine("Ingrese el numero");
+            number = Convert.ToInt32(Console.ReadLine());
+            if (number > 4000)
+            {
+                Console.WriteLine("No puede pasar de 4000");
+            }
+            M = Math.Truncate(number / 1000);
+            C=Math.Truncate(number / 100)%10;
+            D=Math.Truncate(number / 10)%10;
+            u = Math.Truncate(number / 1)%10;
+
+        }
+    }
+}
